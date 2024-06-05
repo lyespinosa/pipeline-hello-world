a1efd1ba2e4cd28a646e3d20069d35ae16d76816
Leonardo Espinosa <leonardo@MacBook-Air-de-Leonardo-2.local>
2024-06-05 15:11:09 -0600

    pipelines 3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api/v1/welcome', (req, res) => {
-    res.send('Hola a las pipelains de Jenkins!');
+    res.send('Hola a las pipelains!');
 });
 
 app.listen(port, () => {
