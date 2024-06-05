0a2ffdc3c338b027f8403bf374cd7f050afd78ca
Leonardo Espinosa <leonardo@MacBook-Air-de-Leonardo-2.local>
2024-06-05 14:11:24 -0600

    add jenkinsdile

--- test.js
+++ test.js
@@ -3,13 +3,13 @@ const express = require('express');
 
 const app = express();
 app.get('/api/v1/welcome', (req, res) => {
-    res.status(200).send('Hola Mundo! como estamos mi gente');
+    res.status(200).send('Hola a las pipelains');
 });
 
 describe('GET /api/v1/welcome', () => {
     it('Todo correcto', (done) => {
         request(app)
             .get('/api/v1/welcome')
-            .expect('Hola Mundo! como estamos mi gente', done);
+            .expect('Hola a las pipelains', done);
     });
 });
